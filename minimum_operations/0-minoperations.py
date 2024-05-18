41601e2c02116acb904ee35522db0fdf62da03f7
ruran8wa <p.rurangwa@alustudent.com>
2024-05-18 02:30:24 -0700

    initial commit

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+i#!/usr/bin/python3
+"""minimum operations"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    ops = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            ops += divisor
+            n = n // divisor
+        else:
+            divisor += 1
+
+    return ops
